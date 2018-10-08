d1a216d0aa38f10a93d57f7da99faeea6feb314b
Liam Dawson <liam@ldaws.com>
2018-10-08 22:13:43 +1100

    improve matcher tests

--- txdo/src/matchers.rs
+++ txdo/src/matchers.rs
@@ -49,16 +49,60 @@ pub fn parse_date(buffer: &[u8]) -> (Option<&[u8]>, &[u8]) {
 mod tests {
     use super::*;
 
+    use std::str;
+
     macro_rules! input {
         ($val:expr) => { $val.as_bytes() }
     }
 
+    fn coerce_to_string(bytes: &[u8]) -> &str {
+        str::from_utf8(bytes).unwrap()
+    }
+
+    #[test]
     pub fn completed_cases() {
         assert_eq!(parse_completed(input!("x ")).0, true);
 
+        // must have a space following
         assert_eq!(parse_completed(input!("x")).0, false);
         assert_eq!(parse_completed(input!("xylophone")).0, false);
+        // can't be preceded by a space
         assert_eq!(parse_completed(input!(" x")).0, false);
+        // can't be uppercase
         assert_eq!(parse_completed(input!("X ")).0, false);
+    }
+
+    #[test]
+    pub fn priority_cases() {
+        assert_eq!(parse_priority(input!("(A) ")).0, Some('A' as u8));
+        assert_eq!(parse_priority(input!("(X) ")).0, Some('X' as u8));
+
+        // can't be lowercase
+        assert_eq!(parse_priority(input!("(c) ")).0, None);
+        // can't be preceded by a space
+        assert_eq!(parse_priority(input!(" (c)")).0, None);
+        assert_eq!(parse_priority(input!(" (O).(o)")).0, None);
+    }
+
+    #[test]
+    pub fn date_cases() {
+        // TODO: fix and test non-date formats
+        //       e.g. 2018-99-99
+
+        assert_eq!(parse_date(input!("2018-01-01 ")).0.map(coerce_to_string), Some("2018-01-01"));
+
+        // this should pass
+        // assert_eq!(parse_date(input!("2018-99-99 ")).0.map(coerce_to_string), None);
+
+        // dates should be followed by spaces
+        assert_eq!(parse_date(input!("2018-01-01")).0.map(coerce_to_string), None);
+
+        // dates must use four/two/two-digit components
+        assert_eq!(parse_date(input!("18-01-01 ")).0.map(coerce_to_string), None);
+        assert_eq!(parse_date(input!("2018-1-01 ")).0.map(coerce_to_string), None);
+        assert_eq!(parse_date(input!("2018-01-1 ")).0.map(coerce_to_string), None);
+
+        // dates must use hyphens as separators
+        assert_eq!(parse_date(input!("2018_01_01 ")).0.map(coerce_to_string), None);
     }
 }
